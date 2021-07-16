ea3c57381bfd38cf72d124fb900d7d8a36f4d064
Agit <csd@hotmail.com.tr>
2021-07-16 23:39:21 +0100

    ebay

--- ebay/addInventory/__init__.py
+++ ebay/addInventory/__init__.py
@@ -1,123 +1,123 @@
 import logging
 
 import azure.functions as func
-import time
-import openapi_client
-from pprint import pprint
-from openapi_client.api import inventory_item_api
-from openapi_client.model.availability import Availability
-from openapi_client.model.availability_distribution import AvailabilityDistribution
-from openapi_client.model.base_response import BaseResponse
-from openapi_client.model.bulk_get_inventory_item import BulkGetInventoryItem
-from openapi_client.model.bulk_get_inventory_item_response import BulkGetInventoryItemResponse
-from openapi_client.model.bulk_inventory_item import BulkInventoryItem
-from openapi_client.model.bulk_inventory_item_response import BulkInventoryItemResponse
-from openapi_client.model.bulk_price_quantity import BulkPriceQuantity
-from openapi_client.model.bulk_price_quantity_response import BulkPriceQuantityResponse
-from openapi_client.model.dimension import Dimension
-from openapi_client.model.inventory_item import InventoryItem
-from openapi_client.model.inventory_item_with_sku_locale import InventoryItemWithSkuLocale
-from openapi_client.model.inventory_item_with_sku_locale_groupid import InventoryItemWithSkuLocaleGroupid
-from openapi_client.model.inventory_items import InventoryItems
-from openapi_client.model.package_weight_and_size import PackageWeightAndSize
-from openapi_client.model.pickup_at_location_availability import PickupAtLocationAvailability
-from openapi_client.model.product import Product
-from openapi_client.model.ship_to_location_availability import ShipToLocationAvailability
-from openapi_client.model.time_duration import TimeDuration
-from openapi_client.model.weight import Weight
+# import time
+# import openapi_client
+# from pprint import pprint
+# from openapi_client.api import inventory_item_api
+# from openapi_client.model.availability import Availability
+# from openapi_client.model.availability_distribution import AvailabilityDistribution
+# from openapi_client.model.base_response import BaseResponse
+# from openapi_client.model.bulk_get_inventory_item import BulkGetInventoryItem
+# from openapi_client.model.bulk_get_inventory_item_response import BulkGetInventoryItemResponse
+# from openapi_client.model.bulk_inventory_item import BulkInventoryItem
+# from openapi_client.model.bulk_inventory_item_response import BulkInventoryItemResponse
+# from openapi_client.model.bulk_price_quantity import BulkPriceQuantity
+# from openapi_client.model.bulk_price_quantity_response import BulkPriceQuantityResponse
+# from openapi_client.model.dimension import Dimension
+# from openapi_client.model.inventory_item import InventoryItem
+# from openapi_client.model.inventory_item_with_sku_locale import InventoryItemWithSkuLocale
+# from openapi_client.model.inventory_item_with_sku_locale_groupid import InventoryItemWithSkuLocaleGroupid
+# from openapi_client.model.inventory_items import InventoryItems
+# from openapi_client.model.package_weight_and_size import PackageWeightAndSize
+# from openapi_client.model.pickup_at_location_availability import PickupAtLocationAvailability
+# from openapi_client.model.product import Product
+# from openapi_client.model.ship_to_location_availability import ShipToLocationAvailability
+# from openapi_client.model.time_duration import TimeDuration
+# from openapi_client.model.weight import Weight
 
-configuration = openapi_client.Configuration(
-    host = "https://api.sandbox.ebay.com/sell/inventory/v1"
-    # host = "https://api.ebay.com/sell/inventory/v1"
-)
-configuration.access_token = 'v^1.1#i^1#p^3#r^0#f^0#I^3#t^H4sIAAAAAAAAAOVZW4zc1Bne2VtIQhIqkRRRKk0dVHGpPfbY47EtZqRJdjY7SXZnWO8uISJsz9jHs4d4bNfneLNTQbVaqWmBtxYq7loQFwlBgCAKqQhE8ABVH1oKAQSoRWqRoBKBXhAtiJZj7+5kdmiTnRkeRuq8jM7xf/v+yzn/b/Pzg+svOzxy+NNNsXW9i/P8fG8sJmzk1w8OXL65r/fCgR6+gSC2OH/xfP9C3/tXYFC1PW0cYs91MIzPVW0Ha9Fmhgl8R3MBRlhzQBVijRianhvdqyU5XvN8l7iGazPxwlCGgUpZShsqEE3B4A1TprvOiswJN8OYSSsJhbSkyqYlSrJBn2McwIKDCXBIhknySYHl06wgTQiyJqmawHOqIO9n4lPQx8h1KAnHM9nIXC3i9RtsPbOpAGPoEyqEyRZyw3oxVxjKj01ckWiQlV32g04ACfDq1U7XhPEpYAfwzGpwRK3pgWFAjJlEdknDaqFabsWYNsyPXA3UspgWLV4yhLKlysrX4sph168CcmY7wh1kslZEqkGHIFI7m0epN8rXQYMsr8aoiMJQPPy7MgA2shD0M0x+R+7qST0/zsT1Usl3Z5EJzRCpIEpiSkomFSZLIKYuhP40cmfBsp4lYcteblK003VMFPoMx8dcsgNSo2Gza8QG11CiolP0cxYJDWqkU+ouTO4PY7oUxIDMOGFYYZX6IR4tzx6AlYw4nQNfW07QfBCttCULpgnU/5YSYa23nBbZMDK5UikRmgLLoMZWgX8QEs8GBmQN6t2gCn1kamLKSoqKBVlTVi1WUi2LLadMmRUsCHkIy2VDVf6PsoMQH5UDAusZ0vwgwphhdMP1YMm1kVFjmkmiA2c5H+ZwhpkhxNMSiUOHDnGHRM71K4kkzwuJfaN7dWMGVgFTp0VnJ2ZRlBoGpFwYaaTmUWvmaOJR5U6FyYq+WQI+qe0IanStQ9umfyvJu8rCbPPu/4C600bUDxNUUXchHXExgWZH0Gy3gpxRSGZcszuwhbW+gi+snMJQR/hynleoVgMCyjYsdAnEeqKKqqLKHcELzzUNAUsj7kHodF+GjueHx/P6yPREcU9+rCOkOjR8SLoLXTElS46cnEznd43NjM2NF3K+LOQ9/Wp5v23tlpTALI6MTY6Mewk+0xH40QrqstxlhY4A5SsBRRTWelehsuS0aCiCKaRVHkgpAMsSnwamasmSqqZToOOjqMuimKsgUtALe9hCcSo3QRsRVt+xj7VkFRpyWYGsYopQgimjI9w4bBS6C3fIj6kA4CEuPEI5w60mXEA74XBrOrI4vhaiRDmoUf0m9DkfAtN17Nra+SoB9fgS99qYMO1luKUulsJoUeNq5hZ4kDNLux/Xr7WjsM4c1noLfMAw3MAh7ahcZm2BwwpsC9l22Oy2o7CBvRUzHWDXCDJw+3GMRhnqYowqM6RVOXSPzj+U3wAE0DawjQTGM67nhZlo0H67hXqxLFovIDCiqbE1Y+kAFc3u7YKt89NTAtkdS/FmXAd2LAWYpg9x2wGsywlH7Y6FLL0JaqsOkBOeuTge1noLRwQdMznTB1Yr1eOBWlSuJsJeeNW0pq4Fch9S+WDtmdrEVPdjR1eoD03kQ4NMBz7qrps07CCmoxaiqZdgsV+rGTXYEe7Qt900nK6gLuV0/arieGej6RCc7bZ+UJFMS5CsFAskRWXDBasCJc2mJCvNK0nFMNLNrxv6F2Lfaw03Al02xglyUk4qgiipa41n00bDq7KvvCNNrP5Gke2JfsJC7Di/EDvWG4vxaZ4VLucvHeyb7O87l8H0fOIwcMyyO8fRyZ6jl7tDT2QfcgdhzQPI7x2MobdeM/7Z8HVk8QB/Qf37yPo+YWPDxxL+otNPBoQt39yUFPi0IAl0pBH4/fz200/7hW39589+vnXfPS8e+9eRUy8+cFTHN+955bYBflOdKBYb6KEh77nuqnWidM4dP3vyxhuOFMvWie9XnvrNvHDZkVueXLfxd8XvTt6c/kH/Szc93XPgzYvuvPQX+6bm/rC39J2jT0yyvz1+cf4wuSS/a2TwmWvkJ/68oXTsqc8X9544cG1qC7vhxLfeG77zG97tR3bt/uGFf0n+6oGJv0/1XXDw5L1bP3ga3f3rbe+c+tje9cLUn7S/zW+bvGuRv/bR507ecet5z5/7xo/fnn/1948PbbhkdOG9lz+6590b/33/a5v3XMNcf5/8be7ZrW8/bFZPLGwc+/iF23/OHf/rT91/PLv+pj+evP7u7T/a9p9flm/5yWO8/VBm4fW4d/K293t2LiSszQwOhr5Infrkld1XPvPhre/2cPYjdz34wYNbjt4w/Nn2pTB+CUkD9Rm3GgAA'
+# configuration = openapi_client.Configuration(
+#     host = "https://api.sandbox.ebay.com/sell/inventory/v1"
+#     # host = "https://api.ebay.com/sell/inventory/v1"
+# )
+# configuration.access_token = 'v^1.1#i^1#p^3#r^0#f^0#I^3#t^H4sIAAAAAAAAAOVZW4zc1Bne2VtIQhIqkRRRKk0dVHGpPfbY47EtZqRJdjY7SXZnWO8uISJsz9jHs4d4bNfneLNTQbVaqWmBtxYq7loQFwlBgCAKqQhE8ABVH1oKAQSoRWqRoBKBXhAtiJZj7+5kdmiTnRkeRuq8jM7xf/v+yzn/b/Pzg+svOzxy+NNNsXW9i/P8fG8sJmzk1w8OXL65r/fCgR6+gSC2OH/xfP9C3/tXYFC1PW0cYs91MIzPVW0Ha9Fmhgl8R3MBRlhzQBVijRianhvdqyU5XvN8l7iGazPxwlCGgUpZShsqEE3B4A1TprvOiswJN8OYSSsJhbSkyqYlSrJBn2McwIKDCXBIhknySYHl06wgTQiyJqmawHOqIO9n4lPQx8h1KAnHM9nIXC3i9RtsPbOpAGPoEyqEyRZyw3oxVxjKj01ckWiQlV32g04ACfDq1U7XhPEpYAfwzGpwRK3pgWFAjJlEdknDaqFabsWYNsyPXA3UspgWLV4yhLKlysrX4sph168CcmY7wh1kslZEqkGHIFI7m0epN8rXQYMsr8aoiMJQPPy7MgA2shD0M0x+R+7qST0/zsT1Usl3Z5EJzRCpIEpiSkomFSZLIKYuhP40cmfBsp4lYcteblK003VMFPoMx8dcsgNSo2Gza8QG11CiolP0cxYJDWqkU+ouTO4PY7oUxIDMOGFYYZX6IR4tzx6AlYw4nQNfW07QfBCttCULpgnU/5YSYa23nBbZMDK5UikRmgLLoMZWgX8QEs8GBmQN6t2gCn1kamLKSoqKBVlTVi1WUi2LLadMmRUsCHkIy2VDVf6PsoMQH5UDAusZ0vwgwphhdMP1YMm1kVFjmkmiA2c5H+ZwhpkhxNMSiUOHDnGHRM71K4kkzwuJfaN7dWMGVgFTp0VnJ2ZRlBoGpFwYaaTmUWvmaOJR5U6FyYq+WQI+qe0IanStQ9umfyvJu8rCbPPu/4C600bUDxNUUXchHXExgWZH0Gy3gpxRSGZcszuwhbW+gi+snMJQR/hynleoVgMCyjYsdAnEeqKKqqLKHcELzzUNAUsj7kHodF+GjueHx/P6yPREcU9+rCOkOjR8SLoLXTElS46cnEznd43NjM2NF3K+LOQ9/Wp5v23tlpTALI6MTY6Mewk+0xH40QrqstxlhY4A5SsBRRTWelehsuS0aCiCKaRVHkgpAMsSnwamasmSqqZToOOjqMuimKsgUtALe9hCcSo3QRsRVt+xj7VkFRpyWYGsYopQgimjI9w4bBS6C3fIj6kA4CEuPEI5w60mXEA74XBrOrI4vhaiRDmoUf0m9DkfAtN17Nra+SoB9fgS99qYMO1luKUulsJoUeNq5hZ4kDNLux/Xr7WjsM4c1noLfMAw3MAh7ahcZm2BwwpsC9l22Oy2o7CBvRUzHWDXCDJw+3GMRhnqYowqM6RVOXSPzj+U3wAE0DawjQTGM67nhZlo0H67hXqxLFovIDCiqbE1Y+kAFc3u7YKt89NTAtkdS/FmXAd2LAWYpg9x2wGsywlH7Y6FLL0JaqsOkBOeuTge1noLRwQdMznTB1Yr1eOBWlSuJsJeeNW0pq4Fch9S+WDtmdrEVPdjR1eoD03kQ4NMBz7qrps07CCmoxaiqZdgsV+rGTXYEe7Qt900nK6gLuV0/arieGej6RCc7bZ+UJFMS5CsFAskRWXDBasCJc2mJCvNK0nFMNLNrxv6F2Lfaw03Al02xglyUk4qgiipa41n00bDq7KvvCNNrP5Gke2JfsJC7Di/EDvWG4vxaZ4VLucvHeyb7O87l8H0fOIwcMyyO8fRyZ6jl7tDT2QfcgdhzQPI7x2MobdeM/7Z8HVk8QB/Qf37yPo+YWPDxxL+otNPBoQt39yUFPi0IAl0pBH4/fz200/7hW39589+vnXfPS8e+9eRUy8+cFTHN+955bYBflOdKBYb6KEh77nuqnWidM4dP3vyxhuOFMvWie9XnvrNvHDZkVueXLfxd8XvTt6c/kH/Szc93XPgzYvuvPQX+6bm/rC39J2jT0yyvz1+cf4wuSS/a2TwmWvkJ/68oXTsqc8X9544cG1qC7vhxLfeG77zG97tR3bt/uGFf0n+6oGJv0/1XXDw5L1bP3ga3f3rbe+c+tje9cLUn7S/zW+bvGuRv/bR507ecet5z5/7xo/fnn/1948PbbhkdOG9lz+6590b/33/a5v3XMNcf5/8be7ZrW8/bFZPLGwc+/iF23/OHf/rT91/PLv+pj+evP7u7T/a9p9flm/5yWO8/VBm4fW4d/K293t2LiSszQwOhr5Infrkld1XPvPhre/2cPYjdz34wYNbjt4w/Nn2pTB+CUkD9Rm3GgAA'
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-    with openapi_client.ApiClient(configuration) as api_client:
-        # Create an instance of the API class
-        api_instance = inventory_item_api.InventoryItemApi(api_client)
-        bulk_inventory_item = BulkInventoryItem(
-            requests=[
-                InventoryItemWithSkuLocale(
-                    availability=Availability(
-                        # pickup_at_location_availability=[
-                        #     PickupAtLocationAvailability(
-                        #         availability_type="availability_type_example",
-                        #         fulfillment_time=TimeDuration(
-                        #             unit="unit_example",
-                        #             value=1,
-                        #         ),
-                        #         merchant_location_key="merchant_location_key_example",
-                        #         quantity=1,
-                        #     ),
-                        # ],
-                        ship_to_location_availability=ShipToLocationAvailability(
-                            # availability_distributions=[
-                            #     AvailabilityDistribution(
-                            #         fulfillment_time=TimeDuration(
-                            #             unit="unit_example",
-                            #             value=1,
-                            #         ),
-                            #         merchant_location_key="merchant_location_key_example",
-                            #         quantity=1,
-                            #     ),
-                            # ],
-                            quantity=1,
-                        ),
-                    ),
-                    condition="NEW",
-                    condition_description="condition_description_example",
-                    # locale="en_US",
-                    locale="en_GB",
-                    # package_weight_and_size=PackageWeightAndSize(
-                    #     # dimensions=Dimension(
-                    #     #     height=3.14,
-                    #     #     length=3.14,
-                    #     #     unit="unit_example",
-                    #     #     width=3.14,
-                    #     # ),
-                    #     package_type="",
-                    #     # weight=Weight(
-                    #     #     unit="unit_example",
-                    #     #     value=3.14,
-                    #     # ),
-                    # ),
-                    product=Product(
-                        # aspects=[
-                        # ],
-                        price=10,
-                        brand="markasi",
-                        description="aciklaamar deneme",
-                        # ean=[
-                        #     "8698806201329",
-                        # ],
-                        epid="",
-                        image_urls=[
-                            "https://cdn.pixabay.com/photo/2013/07/12/17/47/test-pattern-152459_960_720.png",
-                        ],
-                        # isbn=[
-                        #     "isbn_example",
-                        # ],
-                        mpn="8698806201329",
-                        subtitle="altbaslik",
-                        title="lan bu bir urun",
-                        # upc=[
-                        #     "",
-                        # ],
-                    ),
-                    sku="8698806201331",
-                ),
-            ],
-        )  # BulkInventoryItem | Details of the inventories with sku and locale
+    # logging.info('Python HTTP trigger function processed a request.')
+    # with openapi_client.ApiClient(configuration) as api_client:
+    #     # Create an instance of the API class
+    #     api_instance = inventory_item_api.InventoryItemApi(api_client)
+    #     bulk_inventory_item = BulkInventoryItem(
+    #         requests=[
+    #             InventoryItemWithSkuLocale(
+    #                 availability=Availability(
+    #                     # pickup_at_location_availability=[
+    #                     #     PickupAtLocationAvailability(
+    #                     #         availability_type="availability_type_example",
+    #                     #         fulfillment_time=TimeDuration(
+    #                     #             unit="unit_example",
+    #                     #             value=1,
+    #                     #         ),
+    #                     #         merchant_location_key="merchant_location_key_example",
+    #                     #         quantity=1,
+    #                     #     ),
+    #                     # ],
+    #                     ship_to_location_availability=ShipToLocationAvailability(
+    #                         # availability_distributions=[
+    #                         #     AvailabilityDistribution(
+    #                         #         fulfillment_time=TimeDuration(
+    #                         #             unit="unit_example",
+    #                         #             value=1,
+    #                         #         ),
+    #                         #         merchant_location_key="merchant_location_key_example",
+    #                         #         quantity=1,
+    #                         #     ),
+    #                         # ],
+    #                         quantity=1,
+    #                     ),
+    #                 ),
+    #                 condition="NEW",
+    #                 condition_description="condition_description_example",
+    #                 # locale="en_US",
+    #                 locale="en_GB",
+    #                 # package_weight_and_size=PackageWeightAndSize(
+    #                 #     # dimensions=Dimension(
+    #                 #     #     height=3.14,
+    #                 #     #     length=3.14,
+    #                 #     #     unit="unit_example",
+    #                 #     #     width=3.14,
+    #                 #     # ),
+    #                 #     package_type="",
+    #                 #     # weight=Weight(
+    #                 #     #     unit="unit_example",
+    #                 #     #     value=3.14,
+    #                 #     # ),
+    #                 # ),
+    #                 product=Product(
+    #                     # aspects=[
+    #                     # ],
+    #                     price=10,
+    #                     brand="markasi",
+    #                     description="aciklaamar deneme",
+    #                     # ean=[
+    #                     #     "8698806201329",
+    #                     # ],
+    #                     epid="",
+    #                     image_urls=[
+    #                         "https://cdn.pixabay.com/photo/2013/07/12/17/47/test-pattern-152459_960_720.png",
+    #                     ],
+    #                     # isbn=[
+    #                     #     "isbn_example",
+    #                     # ],
+    #                     mpn="8698806201329",
+    #                     subtitle="altbaslik",
+    #                     title="lan bu bir urun",
+    #                     # upc=[
+    #                     #     "",
+    #                     # ],
+    #                 ),
+    #                 sku="8698806201331",
+    #             ),
+    #         ],
+    #     )  # BulkInventoryItem | Details of the inventories with sku and locale
 
-        try:
-            api_response = api_instance.bulk_create_or_replace_inventory_item(bulk_inventory_item)
-            pprint(api_response)
-        except openapi_client.ApiException as e:
-            print("Exception when calling InventoryItemApi->bulk_create_or_replace_inventory_item: %s\n" % e)
+    #     try:
+    #         api_response = api_instance.bulk_create_or_replace_inventory_item(bulk_inventory_item)
+    #         pprint(api_response)
+    #     except openapi_client.ApiException as e:
+    #         print("Exception when calling InventoryItemApi->bulk_create_or_replace_inventory_item: %s\n" % e)
 
     return func.HttpResponse(
-        "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+        "hehe.",
         status_code=200
     )
--- ebay/olditems/__init__.py
+++ ebay/olditems/__init__.py
@@ -1,45 +1,45 @@
 import logging
 
 import azure.functions as func
-import json
-import openapi_client
-from pprint import pprint
-from openapi_client.api import inventory_item_api
-from openapi_client.model.availability import Availability
-from openapi_client.model.availability_distribution import AvailabilityDistribution
-from openapi_client.model.base_response import BaseResponse
-from openapi_client.model.bulk_get_inventory_item import BulkGetInventoryItem
-from openapi_client.model.bulk_get_inventory_item_response import BulkGetInventoryItemResponse
-from openapi_client.model.bulk_inventory_item import BulkInventoryItem
-from openapi_client.model.bulk_inventory_item_response import BulkInventoryItemResponse
-from openapi_client.model.bulk_price_quantity import BulkPriceQuantity
-from openapi_client.model.bulk_price_quantity_response import BulkPriceQuantityResponse
-from openapi_client.model.dimension import Dimension
-from openapi_client.model.inventory_item import InventoryItem
-from openapi_client.model.inventory_item_with_sku_locale import InventoryItemWithSkuLocale
-from openapi_client.model.inventory_item_with_sku_locale_groupid import InventoryItemWithSkuLocaleGroupid
-from openapi_client.model.inventory_items import InventoryItems
-from openapi_client.model.package_weight_and_size import PackageWeightAndSize
-from openapi_client.model.pickup_at_location_availability import PickupAtLocationAvailability
-from openapi_client.model.product import Product
-from openapi_client.model.ship_to_location_availability import ShipToLocationAvailability
-from openapi_client.model.time_duration import TimeDuration
-from openapi_client.model.weight import Weight
+# import json
+# import openapi_client
+# from pprint import pprint
+# from openapi_client.api import inventory_item_api
+# from openapi_client.model.availability import Availability
+# from openapi_client.model.availability_distribution import AvailabilityDistribution
+# from openapi_client.model.base_response import BaseResponse
+# from openapi_client.model.bulk_get_inventory_item import BulkGetInventoryItem
+# from openapi_client.model.bulk_get_inventory_item_response import BulkGetInventoryItemResponse
+# from openapi_client.model.bulk_inventory_item import BulkInventoryItem
+# from openapi_client.model.bulk_inventory_item_response import BulkInventoryItemResponse
+# from openapi_client.model.bulk_price_quantity import BulkPriceQuantity
+# from openapi_client.model.bulk_price_quantity_response import BulkPriceQuantityResponse
+# from openapi_client.model.dimension import Dimension
+# from openapi_client.model.inventory_item import InventoryItem
+# from openapi_client.model.inventory_item_with_sku_locale import InventoryItemWithSkuLocale
+# from openapi_client.model.inventory_item_with_sku_locale_groupid import InventoryItemWithSkuLocaleGroupid
+# from openapi_client.model.inventory_items import InventoryItems
+# from openapi_client.model.package_weight_and_size import PackageWeightAndSize
+# from openapi_client.model.pickup_at_location_availability import PickupAtLocationAvailability
+# from openapi_client.model.product import Product
+# from openapi_client.model.ship_to_location_availability import ShipToLocationAvailability
+# from openapi_client.model.time_duration import TimeDuration
+# from openapi_client.model.weight import Weight
 
-configuration = openapi_client.Configuration(
-    host = "https://api.sandbox.ebay.com/sell/inventory/v1"
-    # host = "https://api.ebay.com/sell/inventory/v1"
-)
-configuration.access_token = 'v^1.1#i^1#p^3#r^0#f^0#I^3#t^H4sIAAAAAAAAAOVZXYwbRx0/30cgOgKqRPkohbpLi9Kma8+u1/buEht8OefOtHdnbn0huRJO491Ze3rr3e3ObM5WIuV0KGkfUjU8VLomapuH6kCA1EgUqqpUlAJFQhSCmiBBQYLSCBSoSohE+xLE7N6dc2cgOdt9sIRfrJn9f/3+HzP//y5Y3Lb97uPjx9/ZEXlf/5lFsNgfiQjDYPu2oV0fHOi/ZagPbCCInFm8Y3FwaeAvuwmsWa46jYjr2ARF6zXLJmq4meF8z1YdSDBRbVhDRKW6quUm7lPFGFBdz6GO7lhctDCa4RLQNGUElLIAkQATCtu112WWnAxnCqIkCmXdlARDVlLsMSE+KtiEQptmOBGIAg/SvJAsiaIqAlWQY0kxPctF9yGPYMdmJDHAZUNr1ZDX22Dq9S2FhCCPMiFctpDbq03lCqP5ydLu+AZZ2TU3aBRSn2xe7XEMFN0HLR9dXw0JqVXN13VECBfPrmrYLFTNrRvTgfmhp1NmWVYg0CGUgS6D8nviyr2OV4P0+nYEO9jgzZBURTbFtHEjjzJvlB9AOl1bTTIRhdFo8PdFH1rYxMjLcPmR3IEZLT/NRbVi0XMOYQMZAVIhISWSkijKXJYiwlyIvDnsHIJrelaFrXm5RdEexzZw4DMSnXToCGJGo1bXCBtcw4im7CkvZ9LAoCZdqgRA04XibBDT1SD6tGoHYUU15odouLxxANYz4loOvGc5IZlKwkyYaYmlBBLBf8mJoNbbzotsEJpcsRgPbEFl2OBr0JtH1LWgjnidudevIQ8baiJpignZRLyRUkxeUkyTLyeNFC+YCAGEymVdkf+P0oNSD5d9ipop0vogxJjhNN1xUdGxsN7gWknCE2ctIeokw1UpddV4fGFhIbaQiDleJS4CIMT3T9yn6VVUg1yTFt+YmMdhauiIcRGs0obLrKmzzGPK7QqXTXhGEXq0MeI32FpDlsX+1rN3k4XZ1t3/AXWPhZkfSkxRbyEddwhFRlfQLKeC7QlEq47RG9iCWl/HF1ROYbQrfDnXLdRqPoVlCxV6BGIzURMKaye6ghecayqGpkqdeWT3XoZO5/dO57XxudLUvfnJrpBqSPcQ7S10U8mUZKfEmXR+bLI6WZ8u5LyUkHe1A6lZy/yCJPvG1PjkzPi0GweZrsBPVHCP5S4vdAUoX/EZoqDWewqVmUondFkwhLQCoJSEqCyBNDQUMyUpSjoJuz6KeiyKuQqmBa1wL1+Y2pcrsUaE10b282ZKQXqqLCNeNhJIQkm9K9wkaBR6C3fAT5gA6OJYcITGdKcWdyBrhYOtudDi6FaI4mW/wfQbyIt5CBqObTW2zlfxmcdXubfGRFgvE1vtYhmMNjVuZm6DB9uHWPfjeI1OFDaZg1pvgw/quuPbtBOVa6xtcJi+ZWLLCprdThRuYG/HTBtaDYp10nkcw1GGuZjgSpW2K4ftsfmH8euQQtYGdpDApOq4bpCJOuu326gX02T1An09HBvbM5YNUOHw3inYJj87JbDVtRS36tioaynQMDxEOg5gU04wa3ctZPVVUEd1gO3gzCXRoNbbOCLYmBkzPGi2Uz0ubITlamDiBldNe+raIPcQkw+3nqktTE0/dnWFesjAHtLpnO/h3rpJgw5iLmwhWnoJnniNht5AXeEOfNtLw+k66mJO0740Nd3daDqKDvVaPyhLhilIZpKHkqzwwYJXoJzmk5KZBrIo63q69XXD4FLknvZwY9hjY5yQElMJOZ0StjyLt2xseFX2Hy9J45u/UWT7wp+wFHkRLEWe749EQBrwwi5w17aBmcGBD3CEnU8xAm2j7NRjbLKPscvdZieyh2LzqOFC7PVvi+Dfntff3fB15MxB8LHm95HtA8Lwho8l4NZrT4aED310hyiAtJAURREI8iz49LWng8JHBj88+LL403cee+3Pn19+9urFf/3t3OOXqk+BHU2iSGSoj4W8z/jatx750ck3rrw5f2r/768sV1/KLR2oLw/1Hz43rN152/S+x199+PIf6idL+cGvll+48MfnDk++/8Jvrh44v/PYWLTvMv89WfxE5tnLOU258JlHv/8mX43tvWvhc2evHP31/I/PDz9a14ofP/HgygvmM0ef4e5+aODEPcuXIp9d+cfh4WP5V2cWb6djD9905cGsduRnB0+fWjny2s2Vt66Kt/xk+PTrM6XSG8UHLn7jV/jS/W+/cnF37Owr/7zpO2PnjvT5ux568a07d9ZzTzz9TWHi5OkfHlbqf9du/cHszb94evngX/909uxtOz/582Px/urX7z/6Owe99G62skLGvvz2p5Qn5kZuP/7yL0+tXPjuk6+f0L9ycvDbq2H8NxNvg/C3GgAA'
+# configuration = openapi_client.Configuration(
+#     host = "https://api.sandbox.ebay.com/sell/inventory/v1"
+#     # host = "https://api.ebay.com/sell/inventory/v1"
+# )
+# configuration.access_token = 'v^1.1#i^1#p^3#r^0#f^0#I^3#t^H4sIAAAAAAAAAOVZXYwbRx0/30cgOgKqRPkohbpLi9Kma8+u1/buEht8OefOtHdnbn0huRJO491Ze3rr3e3ObM5WIuV0KGkfUjU8VLomapuH6kCA1EgUqqpUlAJFQhSCmiBBQYLSCBSoSohE+xLE7N6dc2cgOdt9sIRfrJn9f/3+HzP//y5Y3Lb97uPjx9/ZEXlf/5lFsNgfiQjDYPu2oV0fHOi/ZagPbCCInFm8Y3FwaeAvuwmsWa46jYjr2ARF6zXLJmq4meF8z1YdSDBRbVhDRKW6quUm7lPFGFBdz6GO7lhctDCa4RLQNGUElLIAkQATCtu112WWnAxnCqIkCmXdlARDVlLsMSE+KtiEQptmOBGIAg/SvJAsiaIqAlWQY0kxPctF9yGPYMdmJDHAZUNr1ZDX22Dq9S2FhCCPMiFctpDbq03lCqP5ydLu+AZZ2TU3aBRSn2xe7XEMFN0HLR9dXw0JqVXN13VECBfPrmrYLFTNrRvTgfmhp1NmWVYg0CGUgS6D8nviyr2OV4P0+nYEO9jgzZBURTbFtHEjjzJvlB9AOl1bTTIRhdFo8PdFH1rYxMjLcPmR3IEZLT/NRbVi0XMOYQMZAVIhISWSkijKXJYiwlyIvDnsHIJrelaFrXm5RdEexzZw4DMSnXToCGJGo1bXCBtcw4im7CkvZ9LAoCZdqgRA04XibBDT1SD6tGoHYUU15odouLxxANYz4loOvGc5IZlKwkyYaYmlBBLBf8mJoNbbzotsEJpcsRgPbEFl2OBr0JtH1LWgjnidudevIQ8baiJpignZRLyRUkxeUkyTLyeNFC+YCAGEymVdkf+P0oNSD5d9ipop0vogxJjhNN1xUdGxsN7gWknCE2ctIeokw1UpddV4fGFhIbaQiDleJS4CIMT3T9yn6VVUg1yTFt+YmMdhauiIcRGs0obLrKmzzGPK7QqXTXhGEXq0MeI32FpDlsX+1rN3k4XZ1t3/AXWPhZkfSkxRbyEddwhFRlfQLKeC7QlEq47RG9iCWl/HF1ROYbQrfDnXLdRqPoVlCxV6BGIzURMKaye6ghecayqGpkqdeWT3XoZO5/dO57XxudLUvfnJrpBqSPcQ7S10U8mUZKfEmXR+bLI6WZ8u5LyUkHe1A6lZy/yCJPvG1PjkzPi0GweZrsBPVHCP5S4vdAUoX/EZoqDWewqVmUondFkwhLQCoJSEqCyBNDQUMyUpSjoJuz6KeiyKuQqmBa1wL1+Y2pcrsUaE10b282ZKQXqqLCNeNhJIQkm9K9wkaBR6C3fAT5gA6OJYcITGdKcWdyBrhYOtudDi6FaI4mW/wfQbyIt5CBqObTW2zlfxmcdXubfGRFgvE1vtYhmMNjVuZm6DB9uHWPfjeI1OFDaZg1pvgw/quuPbtBOVa6xtcJi+ZWLLCprdThRuYG/HTBtaDYp10nkcw1GGuZjgSpW2K4ftsfmH8euQQtYGdpDApOq4bpCJOuu326gX02T1An09HBvbM5YNUOHw3inYJj87JbDVtRS36tioaynQMDxEOg5gU04wa3ctZPVVUEd1gO3gzCXRoNbbOCLYmBkzPGi2Uz0ubITlamDiBldNe+raIPcQkw+3nqktTE0/dnWFesjAHtLpnO/h3rpJgw5iLmwhWnoJnniNht5AXeEOfNtLw+k66mJO0740Nd3daDqKDvVaPyhLhilIZpKHkqzwwYJXoJzmk5KZBrIo63q69XXD4FLknvZwY9hjY5yQElMJOZ0StjyLt2xseFX2Hy9J45u/UWT7wp+wFHkRLEWe749EQBrwwi5w17aBmcGBD3CEnU8xAm2j7NRjbLKPscvdZieyh2LzqOFC7PVvi+Dfntff3fB15MxB8LHm95HtA8Lwho8l4NZrT4aED310hyiAtJAURREI8iz49LWng8JHBj88+LL403cee+3Pn19+9urFf/3t3OOXqk+BHU2iSGSoj4W8z/jatx750ck3rrw5f2r/768sV1/KLR2oLw/1Hz43rN152/S+x199+PIf6idL+cGvll+48MfnDk++/8Jvrh44v/PYWLTvMv89WfxE5tnLOU258JlHv/8mX43tvWvhc2evHP31/I/PDz9a14ofP/HgygvmM0ef4e5+aODEPcuXIp9d+cfh4WP5V2cWb6djD9905cGsduRnB0+fWjny2s2Vt66Kt/xk+PTrM6XSG8UHLn7jV/jS/W+/cnF37Owr/7zpO2PnjvT5ux568a07d9ZzTzz9TWHi5OkfHlbqf9du/cHszb94evngX/909uxtOz/582Px/urX7z/6Owe99G62skLGvvz2p5Qn5kZuP/7yL0+tXPjuk6+f0L9ycvDbq2H8NxNvg/C3GgAA'
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-    with openapi_client.ApiClient(configuration) as api_client:
-        # Create an instance of the API class
-        api_instance = inventory_item_api.InventoryItemApi(api_client)
-        items = api_instance.get_inventory_items()
+    # logging.info('Python HTTP trigger function processed a request.')
+    # with openapi_client.ApiClient(configuration) as api_client:
+    #     # Create an instance of the API class
+    #     api_instance = inventory_item_api.InventoryItemApi(api_client)
+    #     items = api_instance.get_inventory_items()
     return func.HttpResponse(
-     f"{items}",
+     f"f",
          status_code=200
     )
